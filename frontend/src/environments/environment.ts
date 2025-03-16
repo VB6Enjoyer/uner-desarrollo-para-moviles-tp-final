8c9e3588efe90d464e53c6eb6b29f1b38386075a
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-03-16 01:04:18 -0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   googleMapsKey: 'AIzaSyAXu6M0VNnn4rZt3nMNktbzGDFdhI_ya6g'
-  // API_URL:'http:localhost:3000/api'
+  API_URL:'http:localhost:3000/api'
 };
 
 /*
