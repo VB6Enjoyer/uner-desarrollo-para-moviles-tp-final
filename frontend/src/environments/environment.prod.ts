d943c33f95743d7ec35b05ac71dcd7f0942249b9
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-03-16 01:03:50 -0300

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsKey: "AIzaSyAXu6M0VNnn4rZt3nMNktbzGDFdhI_ya6g"
+  googleMapsKey: "AIzaSyAXu6M0VNnn4rZt3nMNktbzGDFdhI_ya6g",
+  API_URL:'http:localhost:3000/api'
 };
