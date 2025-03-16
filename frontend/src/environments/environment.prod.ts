ffe55b7053e48be5c666347f90e25b64e0f28b54
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-03-16 01:04:09 -0300

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMapsKey: "AIzaSyAXu6M0VNnn4rZt3nMNktbzGDFdhI_ya6g",
-  API_URL:'http:localhost:3000/api'
+  API_URL: 'https://uner-desarrollo-para-moviles-tp-final.onrender.com'
 };
